23413bdaeba203018ff88b715bc147c683eae7bf
brian y. <brian127@uw.edu>
2025-06-03 17:33:34 -0700

    Added how-to-play screen outline to start screen

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -5,15 +5,31 @@ using UnityEngine.UI;
 public class StartScreenController : MonoBehaviour
 {
     public Button startButton;
+    public Button helpButton;
+    public GameObject instructionPanel;
+    public Button closeButton;
 
     void Start()
     {
         startButton.onClick.AddListener(OnStartButtonClicked);
+        helpButton.onClick.AddListener(OnHelpButtonClicked);
+        closeButton.onClick.AddListener(OnCloseButtonClicked);
+        instructionPanel.SetActive(false);
     }
-
-    // Loads game scene when start button clicked
     void OnStartButtonClicked()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    // Displays how-to-play panel when help button clicked
+    void OnHelpButtonClicked()
+    {
+        instructionPanel.SetActive(true);
+    }
+
+    // Closes how-to-play panel when close button clicked
+    void OnCloseButtonClicked()
+    {
+        instructionPanel.SetActive(false);
     }
 }
